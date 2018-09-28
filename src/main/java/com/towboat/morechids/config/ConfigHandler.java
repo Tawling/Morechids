50d31683f21e5b6d8dd41e64fd4332af095b68d0
Tawling <zippydee111@gmail.com>
2018-09-27 22:13:22 -0700

    Validation

--- src/main/java/com/towboat/morechids/config/ConfigHandler.java
+++ src/main/java/com/towboat/morechids/config/ConfigHandler.java
@@ -1,8 +1,8 @@
 package com.towboat.morechids.config;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.towboat.morechids.tweaker.MorechidDefinition;
 import com.towboat.morechids.tweaker.MorechidRegistry;
 
@@ -10,6 +10,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.HashMap;
 
 /**
  * ConfigHandler.java
@@ -26,7 +28,7 @@ public class ConfigHandler {
                 while (reader.hasNext()) {
                     String name = reader.nextName();
                     MorechidDefinition def = MorechidRegistry.createMorechid(name);
-                    readMorechidObject(reader, def);
+                    readMorechidObject(def, reader);
                 }
                 reader.endObject();
             } finally {
@@ -37,86 +39,141 @@ public class ConfigHandler {
         }
     }
 
-    public static void readMorechidObject(JsonReader reader, MorechidDefinition def) throws IOException {
+    public static final String[] settingFields = new String[]{
+            "manaCost",
+            "maxMana",
+            "timeCost",
+            "cooldown",
+            "range",
+            "rangeY",
+            "particleColor",
+            "playSound",
+            "blockBreakParticles",
+            "rangeCheckInterval"
+    };
+
+    public static void readMorechidObject(MorechidDefinition def, JsonReader reader) throws IOException {
+        HashMap<String, Object> dict = objectToDict(reader);
+        validateAndAssignSettings(def, dict, false);
+        validateAndAssignSettings(def, dict, true);
+    }
+
+    public static HashMap<String, Object> objectToDict(JsonReader reader) throws IOException {
+        HashMap<String, Object> dict = new HashMap<>();
         reader.beginObject();
-        // flags for GOG values to allow overrides.
-        boolean mc = false,
-                tc = false,
-                cd = false,
-                r = false,
-                ry = false,
-                pc = false,
-                ps = false,
-                rci = false,
-                bbp = false,
-                mm = false;
         while (reader.hasNext()) {
             String name = reader.nextName();
-            System.out.print("Name: ");
-            if (name.equals("manaCost")) {
-                def.manaCost = reader.nextInt();
-                if (!mc) def.manaCostGOG = def.manaCost;
-            } else if (name.equals("timeCost")) {
-                def.timeCost = reader.nextInt();
-                if (!tc) def.timeCostGOG = def.timeCost;
-            } else if (name.equals("cooldown")) {
-                def.cooldown = reader.nextInt();
-                if (!cd) def.cooldownGOG = def.cooldown;
-            } else if (name.equals("range")) {
-                def.range = reader.nextInt();
-                if (!r) def.range = def.rangeGOG;
-            } else if (name.equals("rangeY")) {
-                def.rangeY = reader.nextInt();
-                if (!ry) def.rangeY = def.rangeYGOG;
-            } else if (name.equals("particleColor")) {
-                def.particleColor = Integer.parseInt(reader.nextString(), 16);
-                if (!pc) def.particleColorGOG = def.particleColor;
-            } else if (name.equals("playSound")) {
-                def.playSound = reader.nextBoolean();
-                if (!ps) def.playSoundGOG = def.playSound;
-            } else if (name.equals("rangeCheckInterval")) {
-                def.rangeCheckInterval = reader.nextInt();
-                if (!rci) def.rangeCheckIntervalGOG = def.rangeCheckInterval;
-            } else if (name.equals("blockBreakParticles")) {
-                def.blockBreakParticles = reader.nextBoolean();
-                if (!bbp) def.blockBreakParticlesGOG = def.blockBreakParticles;
-            } else if (name.equals("maxMana")) {
-                def.maxMana = reader.nextInt();
-                if (!mm) def.maxManaGOG = def.maxMana;
-            } else if (name.equals("manaCostGOG")) {
-                def.manaCostGOG = reader.nextInt();
-                mc = true;
-            } else if (name.equals("timeCostGOG")) {
-                def.timeCostGOG = reader.nextInt();
-                tc = true;
-            } else if (name.equals("delayGOG")) {
-                def.cooldownGOG = reader.nextInt();
-                cd = true;
-            } else if (name.equals("rangeGOG")) {
-                def.rangeGOG = reader.nextInt();
-                r = true;
-            } else if (name.equals("rangeYGOG")) {
-                def.rangeYGOG = reader.nextInt();
-                ry = true;
-            } else if (name.equals("particleColorGOG")) {
-                def.particleColorGOG = Integer.parseInt(reader.nextString(), 16);
-                pc = true;
-            } else if (name.equals("playSoundGOG")) {
-                def.playSoundGOG = reader.nextBoolean();
-                ps = true;
-            } else if (name.equals("rangeCheckIntervalGOG")) {
-                def.rangeCheckIntervalGOG = reader.nextInt();
-                rci = true;
-            } else if (name.equals("blockBreakParticlesGOG")) {
-                def.blockBreakParticlesGOG = reader.nextBoolean();
-                bbp = true;
-            } else if (name.equals("blockBreakParticlesGOG")) {
-                def.maxManaGOG = reader.nextInt();
-                mm = true;
-            } else {
-                reader.skipValue();
+            JsonToken token = reader.peek();
+            switch (token){
+                case BOOLEAN:
+                    dict.put(name, reader.nextBoolean());
+                    break;
+                case NUMBER:
+                    dict.put(name, reader.nextDouble());
+                    break;
+                case STRING:
+                    dict.put(name, reader.nextString());
+                    break;
+                case NULL:
+                    dict.put(name, null);
+                    reader.nextNull();
+                    break;
+                default:
+                    reader.skipValue();
             }
         }
         reader.endObject();
+        return dict;
+    }
+
+    public static void validateAndAssignSettings(MorechidDefinition def, HashMap<String, Object> dict, boolean gog) {
+        String suffix = gog ? "GOG" : "";
+
+        for (String fname : settingFields) {
+            boolean valid = true;
+            Object val;
+            String fieldName = fname + suffix;
+
+            if (dict.containsKey(fieldName)) {
+                val = dict.get(fieldName);
+            } else {
+                continue;
+            }
+
+            try {
+                Field field = MorechidDefinition.class.getDeclaredField(fieldName);
+
+                switch (fname) {
+                    case "manaCost":
+                    case "timeCost":
+                    case "cooldown":
+                    case "range":
+                    case "rangeY":
+                        if (val instanceof Double) {
+                            // Check for positive integer
+                            double v = (Double) val;
+                            if ( v < 0 || v != ((Double)val).intValue()) {
+                                valid = false;
+                            } else {
+                                val = (int) v;
+                            }
+                        } else {
+                            valid = false;
+                        }
+                        break;
+                    case "maxMana":
+                        if (val instanceof Double) {
+                            double v = (Double) val;
+                            if ( v < MorechidDefinition.class.getDeclaredField("manaCost"+suffix).getInt(def) || v != ((Double)val).intValue()) {
+                                valid = false;
+                            } else {
+                                val = (int) v;
+                            }
+                        } else {
+                            valid = false;
+                        }
+                        break;
+                    case "particleColor":
+                        if (val instanceof String) {
+                            int color = Integer.parseInt((String)val, 16);
+                            val = color;
+                        } else {
+                            valid = false;
+                        }
+                        break;
+                    case "blockBreakParticles":
+                    case "playSound":
+                        if (!(val instanceof Boolean)) {
+                            valid = false;
+                        }
+                        break;
+                    case "rangeCheckInterval":
+                        if (val instanceof Double) {
+                            double v = (Double) val;
+                            if (v < 1 || v > MorechidDefinition.class.getDeclaredField("cooldown"+suffix).getInt(def) || v != ((Double)val).intValue()) {
+                                valid = false;
+                            } else {
+                                val = (int) v;
+                            }
+                        } else {
+                            valid = false;
+                        }
+                        break;
+                    default:
+                        valid = false;
+                }
+                if (!valid) {
+                    throw new Exception("Invalid value " + val + " specified for field " + fieldName + ".");
+                }
+                if (dict.containsKey(fieldName) && dict.get(fieldName) != null) {
+                    field.set(def, val);
+                    if (!gog) {
+                        MorechidDefinition.class.getDeclaredField(fieldName+"GOG").set(def, val);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
