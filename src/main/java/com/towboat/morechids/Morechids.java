72fbd03664b9c40d33fe723646b4d75f398b9f70
Tawling <zippydee111@gmail.com>
2019-05-07 21:10:03 -0700

    update version to 1.3.0

--- src/main/java/com/towboat/morechids/Morechids.java
+++ src/main/java/com/towboat/morechids/Morechids.java
@@ -19,7 +19,7 @@ import java.io.IOException;
 public class Morechids {
     public static final String MODID = "morechids";
     public static final String NAME = "Morechids";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.3.0";
 
     @Mod.Instance(MODID)
     public static Morechids instance;
